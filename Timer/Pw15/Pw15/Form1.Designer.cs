2fda5232db14222edd304036be6259af08fd9d3d
ArtemBoberok <129393819+ArtemBoberok@users.noreply.github.com>
2024-02-21 10:56:28 +0300

    Add files via upload

---
+++ Timer/Pw15/Pw15/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Pw15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(13, 13);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(589, 153);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "00:00:00";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDate.Location = new System.Drawing.Point(12, 180);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(190, 76);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "Дата";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(13, 270);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(130, 47);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Сброс";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimer.Location = new System.Drawing.Point(16, 333);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(147, 63);
+            this.labelTimer.TabIndex = 3;
+            this.labelTimer.Text = "0:0.0";
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonStartStop.Enabled = false;
+            this.buttonStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStartStop.Location = new System.Drawing.Point(162, 270);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(130, 47);
+            this.buttonStartStop.TabIndex = 4;
+            this.buttonStartStop.Text = "Стоп";
+            this.buttonStartStop.UseVisualStyleBackColor = false;
+            this.buttonStartStop.Visible = false;
+            this.buttonStartStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(619, 450);
+            this.Controls.Add(this.buttonStartStop);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Timer/Pw15/Pw15/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pw15
+{
+    public partial class Form1 : Form
+    {
+        private int m, s, ms;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled)
+            {
+                timer2.Stop();
+                buttonStartStop.Text = "Старт";
+            }
+            else
+            {
+                timer2.Start();
+                buttonStartStop.Text = "Стоп";
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ms++;
+            s = ms / 10;
+            m = s / 60;
+            labelTimer.Text = m + ":" + s % 60 + "." + ms % 10;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled)
+            {
+                timer2.Stop();
+            }
+            else
+            {
+                timer2.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+            labelDate.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m = 0; s = 0; ms = 0;
+            labelTimer.Text = "0:0.0";
+        }  
+    }
+}
