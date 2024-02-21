1598a964fbe7d5ad3c115d5a28e5f35577dd2543
ArtemBoberok <129393819+ArtemBoberok@users.noreply.github.com>
2024-02-21 12:20:24 +0300

    Add files via upload

---
+++ Pw21/Pw21/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace Pw21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.dataGridViewGroup = new System.Windows.Forms.DataGridView();
+            this.labelGroup = new System.Windows.Forms.Label();
+            this.labelTerm = new System.Windows.Forms.Label();
+            this.groupBoxObjects = new System.Windows.Forms.GroupBox();
+            this.comboBoxObjects = new System.Windows.Forms.ComboBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonVed = new System.Windows.Forms.Button();
+            this.buttonBall = new System.Windows.Forms.Button();
+            this.comboBoxGroup = new System.Windows.Forms.ComboBox();
+            this.chartBall = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonChart = new System.Windows.Forms.Button();
+            this.buttonExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGroup)).BeginInit();
+            this.groupBoxObjects.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewGroup
+            // 
+            this.dataGridViewGroup.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGroup.Location = new System.Drawing.Point(18, 48);
+            this.dataGridViewGroup.Name = "dataGridViewGroup";
+            this.dataGridViewGroup.Size = new System.Drawing.Size(643, 337);
+            this.dataGridViewGroup.TabIndex = 0;
+            // 
+            // labelGroup
+            // 
+            this.labelGroup.AutoSize = true;
+            this.labelGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGroup.Location = new System.Drawing.Point(14, 14);
+            this.labelGroup.Name = "labelGroup";
+            this.labelGroup.Size = new System.Drawing.Size(67, 20);
+            this.labelGroup.TabIndex = 1;
+            this.labelGroup.Text = "Группа";
+            // 
+            // labelTerm
+            // 
+            this.labelTerm.AutoSize = true;
+            this.labelTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTerm.Location = new System.Drawing.Point(235, 17);
+            this.labelTerm.Name = "labelTerm";
+            this.labelTerm.Size = new System.Drawing.Size(83, 17);
+            this.labelTerm.TabIndex = 2;
+            this.labelTerm.Text = "3 семестр";
+            // 
+            // groupBoxObjects
+            // 
+            this.groupBoxObjects.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBoxObjects.Controls.Add(this.comboBoxObjects);
+            this.groupBoxObjects.Controls.Add(this.labelName);
+            this.groupBoxObjects.Location = new System.Drawing.Point(667, 48);
+            this.groupBoxObjects.Name = "groupBoxObjects";
+            this.groupBoxObjects.Size = new System.Drawing.Size(155, 178);
+            this.groupBoxObjects.TabIndex = 3;
+            this.groupBoxObjects.TabStop = false;
+            this.groupBoxObjects.Text = "Предметы";
+            // 
+            // comboBoxObjects
+            // 
+            this.comboBoxObjects.FormattingEnabled = true;
+            this.comboBoxObjects.Items.AddRange(new object[] {
+            "ТРПП",
+            "ОАиП",
+            "ОБЖ",
+            "Мат. метод",
+            "Граф. диз"});
+            this.comboBoxObjects.Location = new System.Drawing.Point(9, 42);
+            this.comboBoxObjects.Name = "comboBoxObjects";
+            this.comboBoxObjects.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxObjects.TabIndex = 4;
+            this.comboBoxObjects.SelectedIndexChanged += new System.EventHandler(this.comboBoxObjects_SelectedIndexChanged);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 26);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 13);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Название:";
+            // 
+            // buttonVed
+            // 
+            this.buttonVed.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonVed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonVed.Location = new System.Drawing.Point(676, 250);
+            this.buttonVed.Name = "buttonVed";
+            this.buttonVed.Size = new System.Drawing.Size(146, 33);
+            this.buttonVed.TabIndex = 4;
+            this.buttonVed.Text = "Заполнить ведомость";
+            this.buttonVed.UseVisualStyleBackColor = false;
+            this.buttonVed.Click += new System.EventHandler(this.buttonVed_Click);
+            // 
+            // buttonBall
+            // 
+            this.buttonBall.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonBall.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBall.Location = new System.Drawing.Point(676, 301);
+            this.buttonBall.Name = "buttonBall";
+            this.buttonBall.Size = new System.Drawing.Size(146, 33);
+            this.buttonBall.TabIndex = 5;
+            this.buttonBall.Text = "Средний балл";
+            this.buttonBall.UseVisualStyleBackColor = false;
+            this.buttonBall.Click += new System.EventHandler(this.buttonBall_Click);
+            // 
+            // comboBoxGroup
+            // 
+            this.comboBoxGroup.FormattingEnabled = true;
+            this.comboBoxGroup.Items.AddRange(new object[] {
+            "ИП-21-7К",
+            "ИП-21-8К",
+            "ИП-21-9К"});
+            this.comboBoxGroup.Location = new System.Drawing.Point(87, 14);
+            this.comboBoxGroup.Name = "comboBoxGroup";
+            this.comboBoxGroup.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxGroup.TabIndex = 6;
+            this.comboBoxGroup.SelectedIndexChanged += new System.EventHandler(this.comboBoxGroup_SelectedIndexChanged);
+            // 
+            // chartBall
+            // 
+            this.chartBall.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            chartArea1.Name = "ChartArea1";
+            this.chartBall.ChartAreas.Add(chartArea1);
+            this.chartBall.Location = new System.Drawing.Point(18, 402);
+            this.chartBall.Name = "chartBall";
+            this.chartBall.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chartBall.Series.Add(series1);
+            this.chartBall.Size = new System.Drawing.Size(643, 200);
+            this.chartBall.TabIndex = 7;
+            this.chartBall.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            title1.Name = "Title";
+            title1.Text = "Динамика среднего балла";
+            this.chartBall.Titles.Add(title1);
+            // 
+            // buttonChart
+            // 
+            this.buttonChart.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonChart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonChart.Location = new System.Drawing.Point(676, 352);
+            this.buttonChart.Name = "buttonChart";
+            this.buttonChart.Size = new System.Drawing.Size(146, 33);
+            this.buttonChart.TabIndex = 9;
+            this.buttonChart.Text = "График";
+            this.buttonChart.UseVisualStyleBackColor = false;
+            this.buttonChart.Click += new System.EventHandler(this.buttonChart_Click);
+            // 
+            // buttonExcel
+            // 
+            this.buttonExcel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExcel.Location = new System.Drawing.Point(676, 402);
+            this.buttonExcel.Name = "buttonExcel";
+            this.buttonExcel.Size = new System.Drawing.Size(146, 33);
+            this.buttonExcel.TabIndex = 10;
+            this.buttonExcel.Text = "Экспорт в Excel";
+            this.buttonExcel.UseVisualStyleBackColor = false;
+            this.buttonExcel.Click += new System.EventHandler(this.buttonExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(834, 611);
+            this.Controls.Add(this.buttonExcel);
+            this.Controls.Add(this.buttonChart);
+            this.Controls.Add(this.chartBall);
+            this.Controls.Add(this.comboBoxGroup);
+            this.Controls.Add(this.buttonBall);
+            this.Controls.Add(this.buttonVed);
+            this.Controls.Add(this.groupBoxObjects);
+            this.Controls.Add(this.labelTerm);
+            this.Controls.Add(this.labelGroup);
+            this.Controls.Add(this.dataGridViewGroup);
+            this.Name = "Form1";
+            this.Text = "Сводная ведомость";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGroup)).EndInit();
+            this.groupBoxObjects.ResumeLayout(false);
+            this.groupBoxObjects.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewGroup;
+        private System.Windows.Forms.Label labelGroup;
+        private System.Windows.Forms.Label labelTerm;
+        private System.Windows.Forms.GroupBox groupBoxObjects;
+        private System.Windows.Forms.ComboBox comboBoxObjects;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonVed;
+        private System.Windows.Forms.Button buttonBall;
+        private System.Windows.Forms.ComboBox comboBoxGroup;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBall;
+        private System.Windows.Forms.Button buttonChart;
+        private System.Windows.Forms.Button buttonExcel;
+    }
+}
+
---
+++ Pw21/Pw21/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel1 = Microsoft.Office.Interop.Excel;
+
+namespace Pw21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+        int n = 0;  // Строки
+        int m = 1;  // Столбцы
+
+        // Заполнение таблицы студентами
+        private void comboBoxGroup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            n = 0;        
+            dataGridViewGroup.TopLeftHeaderCell.Value = "ФИО";  // Ячейка левого верхнего угла таблицы
+            dataGridViewGroup.RowHeadersWidth = 150;        	// Ширина столбца заголовков строк
+
+            string Group = comboBoxGroup.SelectedItem.ToString();
+            int i = 0;
+            try
+            {
+                StreamReader sr = new StreamReader(Group + ".txt");
+                while (!sr.EndOfStream)
+                {
+                    sr.ReadLine();
+                    n++;
+                }
+                sr.Close();
+                dataGridViewGroup.RowCount = n + 1;    // Строки
+
+                sr = new StreamReader(Group + ".txt");
+                while (!sr.EndOfStream)
+                {
+                    dataGridViewGroup.Rows[i].HeaderCell.Value = sr.ReadLine();
+                    i++;
+                }
+                sr.Close();
+                dataGridViewGroup.Rows[i].HeaderCell.Value = "Средний балл:";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка чтения \n" + ex.ToString());
+            }          
+        }
+
+        // Заполнение 
+        private void comboBoxObjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridViewGroup.ColumnCount = m;
+            dataGridViewGroup.Columns[m-1].HeaderCell.Value = comboBoxObjects.SelectedItem.ToString();
+            m += 1;
+        }
+
+        private void buttonVed_Click(object sender, EventArgs e)
+        {
+            buttonVed.Enabled = false;
+            for (int i = 0; i < n; i++)         //Цикл по строкам таблицы
+            {
+                for (int j = 0; j < m-1; j++)   //Цикл по столбцам таблицы
+                {
+                    dataGridViewGroup.Rows[i].Cells[j].Value = rand.Next(2, 6);
+                }
+            }
+
+            for (int i = 0; i < n; i++)             //Все строки
+                for (int j = 0; j < m - 1; j++)     //Все столбцы
+                {
+                    dataGridViewGroup[j, i].ToolTipText = "Ячейка";       //Текст всплывающей подсказки
+                    dataGridViewGroup[j, i].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                    dataGridViewGroup[j, i].Style.SelectionBackColor = Color.Green;   //Фон выделенной ячейки
+                    dataGridViewGroup[j, i].Style.SelectionForeColor = Color.White;   //Текст выделенной ячейки
+                    if (dataGridViewGroup[j, i].Value.ToString() == "2")              //Проверка значения в ячейке
+                    {
+                        dataGridViewGroup[j, i].Style.BackColor = Color.Red;          //Цвет фон для 5
+                        dataGridViewGroup[j, i].Style.ForeColor = Color.Yellow;       //Цвет текста для 5
+                    }
+                }
+        }
+
+        private void buttonBall_Click(object sender, EventArgs e)
+        {
+            buttonBall.Enabled = false;
+            double[] ball = new double[dataGridViewGroup.ColumnCount];
+            for (int i = 0; i < dataGridViewGroup.ColumnCount; i++)
+            {
+                double result = 0;
+                for (int j = 0; j < dataGridViewGroup.RowCount - 1; j++)
+                {
+                    result+= Convert.ToDouble(dataGridViewGroup[i, j].Value);
+                }
+                ball[i] = result / (dataGridViewGroup.RowCount - 1);
+                dataGridViewGroup[i, dataGridViewGroup.RowCount - 1].Value = Math.Round(ball[i], 2);
+            }
+        }
+
+        private void buttonChart_Click(object sender, EventArgs e)
+        {
+            buttonChart.Enabled = false;
+            chartBall.Series[0].Points.Clear();
+            for(int i =0; i < m -1; i++)
+            {
+                chartBall.Series[0].Points.AddXY(dataGridViewGroup.Columns[i].HeaderCell.Value, dataGridViewGroup[i, dataGridViewGroup.RowCount - 1].Value);
+            }
+        }
+
+        private void buttonExcel_Click(object sender, EventArgs e)
+        {
+            var application = new Excel1.Application();
+            application.SheetsInNewWorkbook = 1;
+            Excel1.Workbook workbook = application.Workbooks.Add(Type.Missing);
+
+            Excel1.Worksheet worksheet = application.Worksheets.Item[1];
+            worksheet.Name = "Оценки";
+            worksheet.Cells[1, 1] = dataGridViewGroup.Columns[0].HeaderCell.Value;
+            worksheet.Cells[1, 2] = dataGridViewGroup.Columns[1].HeaderCell.Value;
+
+            int n = 5;// Fio.Length;
+            for (int i = 2; i < n + 2; i++)
+            {
+                worksheet.Cells[i, 1] = dataGridViewGroup[0, i - 2].Value;
+                worksheet.Cells[i, 2] = dataGridViewGroup[1, i - 2].Value;
+            }
+            application.Visible = true;
+        }
+    }
+}
